4444d1e9dd213cbae4150f4ef70f193ab27c8d54
ninjaginja^C <mollywojcik@Mollys-MacBook-Pro-2.local>
2017-12-09 09:53:16 -0700

    post-tudor session

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,13 +4,27 @@ window.onload = function() {
 numberWins = 0;
 numberLosses = 0;
 guessesLeft = 9;
+guessesSoFar = [];
 
 // Set variable to define all the possible letters to choose from
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // Randomly choose a letter from the letters array.
-var randomLetter = letters[Math.floor(Math.random() * letters.length)];
+var randomLetter = null;
+var updateRandomLetter = function() {
+  randomLetter = letters[Math.floor(Math.random() * letters.length)]
+};
+updateRandomLetter();
 console.log(randomLetter);
+
+// Reset function - empty array of letters and reset guessesLeft
+var reset = function() {
+  guessesLeft = 9;
+  guessesSoFar = [];
+  updateRandomLetter();
+  console.log("test");
+}
+
 
 // Define function to detect when user presses a keystroke
 document.onkeyup = function(event) {
@@ -21,18 +35,21 @@ document.onkeyup = function(event) {
   document.querySelector("#guesses-left").innerHTML = guessesLeft;
 
   // Write chosen randomLetter to "user-input" span id in 'Your Guesses so far' - How do I get this to list the letters rather than overwrite each entry?
-  document.querySelector("#user-input").innerHTML = userInput;
+  document.querySelector("#user-input").innerHTML = guessesSoFar;
+  guessesSoFar.push(userInput);
+  console.log(guessesSoFar);
 
-  // Compare userInput to randomLetter - How do I get the number sequences to reset correctly at end of game?
-  if ((userInput == randomLetter) && (guessesLeft > 0)) {
+  // Compare userInput to randomLetter - Why is there a delay of keystroke in resetting Guesses to 9?
+  if (userInput == randomLetter) {
     numberWins++;
-    guessesLeft = 9;
     console.log(guessesLeft);
     document.querySelector("#wins").innerHTML = numberWins;
-  } else {
+    reset();
+  } if (guessesLeft === 0) {
     numberLosses++;
+    document.querySelector("#losses").innerHTML = numberLosses;
+    reset();
   }
-
 };
 
 };
